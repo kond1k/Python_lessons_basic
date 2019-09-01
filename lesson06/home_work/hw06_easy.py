cfe139be0e43f4256e4519b31291bf17f9a98ea6
kond1k <kond1k12@gmail.com>
2019-09-01 17:03:16 +0300

    Домашняя работа к 6 уроку easy and normal

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,5 +1,32 @@
+import math
+
+
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+
+
+class Triangle:
+
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.x1 = x1
+        self.x2 = x2
+        self.x3 = x3
+        self.y1 = y1
+        self.y2 = y2
+        self.y3 = y3
+
+    def square(self):
+        return math.fabs((self.x2 - self.x1) * (self.y3 - self.y1) - (self.x3 - self.x1) * (self.y2 - self.y1)) / 2
+
+    def height(self):
+        return ((self.x2 - self.x1) ** 2 + (self.y2 - self.y1) ** 2) ** (1 / 2) + (
+                (self.x3 - self.x2) ** 2 + (self.y3 - self.y2) ** 2) ** (1 / 2) + (
+                       (self.x1 - self.x3) ** 2 + (self.y1 - self.y3) ** 2) ** (1 / 2)
+
+
+my_tr = Triangle(2, 1, 6, 5, 5, 10)
+print(my_tr.square())
+print(my_tr.height())
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
@@ -7,3 +34,43 @@
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+class TrapezeIsometric:
+    A = 0
+    B = 0
+    C = 0
+    D = 0
+
+    def __init__(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        self.x1 = x1
+        self.x2 = x2
+        self.x3 = x3
+        self.y1 = y1
+        self.y2 = y2
+        self.y3 = y3
+        self.x4 = x4
+        self.y4 = y4
+        self.A = ((self.x2 - self.x1) ** 2 + (self.y2 - self.y1) ** 2) ** (1 / 2)
+        self.B = ((self.x3 - self.x2) ** 2 + (self.y3 - self.y2) ** 2) ** (1 / 2)
+        self.C = ((self.x4 - self.x3) ** 2 + (self.y4 - self.y3) ** 2) ** (1 / 2)
+        self.D = ((self.x1 - self.x4) ** 2 + (self.y1 - self.y4) ** 2) ** (1 / 2)
+        print(self.A, self.B, self.C, self.D)
+
+    def is_isometric(self):
+        if (self.A == self.C and (self.x4 - self.x1) / self.D == (self.x3 - self.x2) / self.B) or (
+                self.B == self.D and (self.x3 - self.x4) / self.D == (self.x2 - self.x1) / self.B):
+            print('Трапеция равнобочная')
+        else:
+            print('Трапеция не равнобочная')
+
+    def perimeter(self):
+        return self.A + self.B + self.C + self.D
+
+    def square(self):
+        h = (self.A ** 2 - (((self.D - self.B) ** 2 + self.A ** 2 - self.C ** 2) / 2 * (self.D - self.B))) ** 1 / 2
+        return 1 / 2 * (self.A + self.B) * h
+
+
+my_trapeze = TrapezeIsometric(2, 4, 0, 2, 0, 7, 2, 5)
+my_trapeze.is_isometric()
+print(my_trapeze.perimeter())
+print(my_trapeze.square())
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -7,11 +7,111 @@
 # Т.е. Учитель Иванов может преподавать математику у 5А и 6Б,
 # но больше математику не может преподавать никто другой.
 
+
+class School:
+    classes = []
+
+    def __init__(self, classes):
+        self.classes = classes
+
+    def get_all_class(self):
+        print(*self.classes)
+
+
+class Class:
+    pupils = []
+    subjects = {}
+
+    def __init__(self, class_number):
+        self.name = class_number
+
+    def add_pupil(self, pupil):
+        self.pupils.append(pupil)
+        pupil.add_class(self)
+
+    def set_subject(self, teacher):
+        self.subjects[teacher.get_subject] = teacher
+
+    def get_pupils(self):
+        print(f'Ученики {self.name} класса:')
+        for i in self.pupils:
+            print(i.get_name())
+        return self.pupils
+
+    def get_subjects(self):
+        for value in self.subjects.values():
+            print(f'предмет: {value.get_subject()}, учитель: {value.get_name()}')
+
+    def __str__(self):
+        return self.name
+
+
+class People:
+    def __init__(self, sex, name, surname):
+        self.sex = sex
+        self.name = name
+        self.surname = surname
+
+    def get_name(self):
+        return f'{self.name} {self.surname}'
+
+
+class Pupil(People):
+    my_class = None
+
+    def __init__(self, sex, name, surname, parent1, parent2):
+        People.__init__(self, sex, name, surname)
+        self.parent1 = parent1
+        self.parent2 = parent2
+
+    def add_class(self, class_number):
+        self.my_class = class_number
+
+    def get_parents(self):
+        print(f'{self.parent1.get_name()} и {self.parent2.get_name()}')
+
+    def get_info(self):
+        print(f'{self.get_name()} класс {self.my_class}')
+        self.my_class.get_subjects()
+
+
+class Teacher(People):
+    def __init__(self, sex, name, surname, subject):
+        People.__init__(self, sex, name, surname)
+        self.subject = subject
+
+    def get_subject(self):
+        return self.subject
+
+
+class_7A = Class('7A')
+class_7B = Class('7B')
+class_7C = Class('7C')
+school_15 = School([class_7A, class_7B, class_7C])
+maria_ivanovna = Teacher('F', 'Мария', 'Герасимовна', 'Математика')
+ludmila_petrovna = Teacher('F', 'Людьмила', 'Петровна', 'Русский')
+masha = People('F', 'Маша', 'Иванова')
+igor = People('M', 'Игорь', 'Иванов')
+ivan = Pupil('M', 'Иван', 'Иванов', masha, igor)
+class_7A.add_pupil(ivan)
+class_7A.set_subject(ludmila_petrovna)
+class_7A.set_subject(maria_ivanovna)
+
 # Выбранная и заполненная данными структура должна решать следующие задачи:
 # 1. Получить полный список всех классов школы
+school_15.get_all_class()
+print('*********')
 # 2. Получить список всех учеников в указанном классе
+class_7A.get_pupils()
+print('*********')
 #  (каждый ученик отображается в формате "Фамилия И.О.")
 # 3. Получить список всех предметов указанного ученика 
 #  (Ученик --> Класс --> Учителя --> Предметы)
+ivan.get_info()
+print('*********')
 # 4. Узнать ФИО родителей указанного ученика
+ivan.get_parents()
+print('*********')
 # 5. Получить список всех Учителей, преподающих в указанном классе
+class_7A.get_subjects()
+
